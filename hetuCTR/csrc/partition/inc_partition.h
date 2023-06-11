e444ae9db0441307a6a3db62cb3c52ef8e8212dd
Your Name <youd@example.com>
2023-06-12 00:17:03 +0800

    init inc_partition

---
+++ hetuCTR/csrc/partition/inc_partition.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "partition.h"
+#include "core/types.h"
+#include "pybind/pybind.h"
+
+namespace hetuCTR {
+class IncPartitionStruct :public PartitionStruct {
+public:
+    IncPartitionStruct(const py::array_t<int>& _input_data, const py::array_t<float>& _comm_mat, int _n_part, int _batch_size, float _theta) : \
+        PartitionStruct(_input_data, _comm_mat, _n_part, _batch_size, _theta) {}
+};
+
+std::unique_ptr<IncPartitionStruct> inc_partition(
+  const py::array_t<int>& _input_data,
+  const py::array_t<float>& _comm_mat,
+  int n_part, int batch_size, float theta
+);
+
+}// namespace hetuCTR
