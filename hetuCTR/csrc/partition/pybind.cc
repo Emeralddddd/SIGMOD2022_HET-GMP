58e02f909965541dc6da86529767a39a2a4fb86a
Your Name <youd@example.com>
2023-06-12 00:16:49 +0800

    change privat to protected

--- hetuCTR/csrc/partition/pybind.cc
+++ hetuCTR/csrc/partition/pybind.cc
@@ -1,5 +1,6 @@
 #include "pybind/pybind.h"
 #include "partition.h"
+#include "inc_partition.h"
 
 namespace hetuCTR {
 
@@ -18,6 +19,20 @@ PYBIND11_MODULE(hetuCTR_partition, m) {
       return bind::vec_nocp(s.cnt_embed_);
     });
   m.def("partition", partition);
+
+  py::class_<IncPartitionStruct>(m, "_IncPartitionStruct", py::module_local())
+    .def("refine_data", &PartitionStruct::refineData)
+    .def("refine_embed", &PartitionStruct::refineEmbed)
+    .def("get_communication", &PartitionStruct::getCommunication)
+    .def("get_priority", &PartitionStruct::getPriority)
+    .def("get_result", &PartitionStruct::getResult)
+    .def("get_data_cnt", [](IncPartitionStruct &s) {
+      return bind::vec_nocp(s.cnt_data_);
+    })
+    .def("get_embed_cnt", [](IncPartitionStruct &s) {
+      return bind::vec_nocp(s.cnt_embed_);
+    });
+  m.def("inc_partition", inc_partition);
 
 } // PYBIND11_MODULE
 
