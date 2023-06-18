323842f4e59f6829b4b52e37ed7540d645e16232
Your Name <youd@example.com>
2023-06-18 13:57:21 +0800

    restore partition

--- hetuCTR/csrc/partition/inc_partition.h
+++ hetuCTR/csrc/partition/inc_partition.h
@@ -6,9 +6,13 @@
 namespace hetuCTR {
 class IncPartitionStruct :public PartitionStruct {
 public:
-    IncPartitionStruct(const py::array_t<int>& _input_data, const py::array_t<float>& _comm_mat, int _n_part, int _batch_size, float _theta) : \
-        PartitionStruct(_input_data, _comm_mat, _n_part, _batch_size, _theta) {}
+    IncPartitionStruct(const py::array_t<int>& _input_data, const py::array_t<float>& _comm_mat, int _n_part, int _batch_size, float _theta);
     void savePartialResult(std::string);
+    std::vector<int> res_embed_remaped_;
+    std::unordered_map<int,int> g2l_, l2g_;
+    py::array_t<float> getPriority();
+    py::tuple getResult();
+    int max_embed_;
 };
 
 std::unique_ptr<IncPartitionStruct> inc_partition(
@@ -16,5 +20,4 @@ std::unique_ptr<IncPartitionStruct> inc_partition(
   const py::array_t<float>& _comm_mat,
   int n_part, int batch_size, float theta
 );
-
 }// namespace hetuCTR
--- hetuCTR/csrc/partition/partition.h
+++ hetuCTR/csrc/partition/partition.h
@@ -7,28 +7,25 @@ namespace hetuCTR {
 
 class PartitionStruct {
 public:
+  PartitionStruct(int _n_part, int _batch_size, float _theta) : n_part_(_n_part), batch_size_(_batch_size), theta_(_theta) {}
   PartitionStruct(const py::array_t<int>& _input_data, const py::array_t<float>& _comm_mat, int _n_part, int _batch_size, float _theta);
   void refineData();
   void refineEmbed();
   py::array_t<float> getCommunication();
   py::array_t<float> getPriority();
-  py::tuple getResult();
 
-  std::vector<int> res_data_, res_embed_, res_embed_remaped_;
+  std::vector<int> res_data_, res_embed_;
   std::vector<int> cnt_data_, cnt_embed_;
 protected:
   float alpha_, beta_, theta_;
 
-  int n_part_, n_data_, n_slot_, n_edge_, n_embed_, max_embed_;
+  int n_part_, n_data_, n_slot_, n_edge_, n_embed_;
   int batch_size_;
   std::vector<int> embed_indptr_, embed_indices_, data_indptr_, data_indices_;
   std::vector<float> soft_cnt_, embed_weight_;
   std::vector<std::vector<int>> cnt_part_embed_;
 
   std::vector<std::vector<float>> comm_mat_;
-
-  std::unordered_map<int,int> g2l_;
-  std::unordered_map<int,int> l2g_;
 };
 
 std::unique_ptr<PartitionStruct> partition(
